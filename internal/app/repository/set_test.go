a8a57b434201ad6a3c55a9fd672c82a8688b81bf
oprekable <oprekable@gmail.com>
2025-03-24 04:04:50 +0700

    add unit tests

---
+++ internal/app/repository/set_test.go
@@ -0,0 +1,43 @@
+package repository
+
+import (
+	"reflect"
+	"simple-reconciliation-service/internal/app/repository/process"
+	mockprocess "simple-reconciliation-service/internal/app/repository/process/_mock"
+	"simple-reconciliation-service/internal/app/repository/sample"
+	mocksample "simple-reconciliation-service/internal/app/repository/sample/_mock"
+	"testing"
+)
+
+func TestNewRepositories(t *testing.T) {
+	type args struct {
+		repoSample  sample.Repository
+		repoProcess process.Repository
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want *Repositories
+	}{
+		{
+			name: "Ok",
+			args: args{
+				repoSample:  mocksample.NewRepository(t),
+				repoProcess: mockprocess.NewRepository(t),
+			},
+			want: &Repositories{
+				RepoSample:  mocksample.NewRepository(t),
+				RepoProcess: mockprocess.NewRepository(t),
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := NewRepositories(tt.args.repoSample, tt.args.repoProcess); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("NewRepositories() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
